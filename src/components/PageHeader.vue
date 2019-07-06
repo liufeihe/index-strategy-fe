a468bc9695df27cf13cd08921fe6e19ce2baeac9
liufeihe <lfhe2014@163.com>
2019-07-06 13:00:24 +0800

    first blood

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="page-header">
+    <div class="wrapper">
+      <div class="web-title">RichTech</div>
+      <div class="user-cnt">
+        <div class="user-info" v-if="isLogin">
+          <img class="user-avatar" src="../assets/images/user_default_grey.png">
+          <span class="user-name">Anonymous</span>
+        </div>
+        <el-link v-else>登录</el-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      isLogin: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../assets/scss/components/PageHeader.scss"
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <PageHeader></PageHeader>
+    <div class="img-wrap">
+      <div class="text-wrap">
+        <p class="title">1. 定投指数基金有什么好？</p>
+        <ul class="desc">
+          <li>指数长期是看涨的，而现在处于低位，牛市来了指数是不会缺席的；</li>
+          <li>指数是永久的，可以躲避黑天鹅，避免在个股上的极大损失；</li>
+          <li>定投是最省心最稳妥的投资办法，可以让你实现稳稳的收益。</li>
+        </ul>
+        <p class="title">2. 定投增强策略是干什么的？</p>
+        <ul class="desc">
+          <li>在低估值区间，多投入点资金；</li>
+          <li>在正常估值值区间，少投入点资金；</li>
+          <li>在高估值区间，分批卖出，从而获取超额收益。</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PageHeader from '../components/PageHeader'
+
+export default {
+  data () {
+    return {}
+  },
+  components: {
+    'PageHeader': PageHeader
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/pages/Home.scss"
+</style>
