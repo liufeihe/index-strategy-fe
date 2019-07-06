7c08e274ac610f01747976a85140b941330cb3ab
liufeihe <lfhe2014@163.com>
2019-07-06 21:06:49 +0800

    add login dialog component

---
+++ src/components/LoginDialog.vue
@@ -0,0 +1,61 @@
+<template>
+  <el-dialog title="用户登录" :visible.sync="isVisible">
+  <el-form :model="form">
+    <el-form-item label="用户名" :label-width="formLabelWidth">
+      <el-input v-model="form.name" autocomplete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="密码" :label-width="formLabelWidth">
+      <el-input v-model="form.password" type="password" autocomplete="off"></el-input>
+    </el-form-item>
+  </el-form>
+  <div slot="footer" class="dialog-footer">
+    <el-button @click="closeDialog()">取 消</el-button>
+    <el-button type="primary" @click="loginUser()">确 定</el-button>
+  </div>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    loginDialogVisible: Boolean
+  },
+  data () {
+    return {
+      formLabelWidth: '60px',
+      form: {
+        name: '',
+        password: ''
+      }
+    }
+  },
+  created () {
+    this.form.name = ''
+    this.form.password = ''
+  },
+  methods: {
+    closeDialog: function () {
+      this.isVisible = false
+    },
+    loginUser: function () {
+
+    }
+  },
+  computed: {
+    isVisible: {
+      get: function () {
+        return this.loginDialogVisible
+      },
+      set: function (newVal) {
+        // console.log('this', this)
+        // console.log('this', this.formLabelWidth)
+        this.$emit('update:loginDialogVisible', newVal)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -3,21 +3,52 @@
     <div class="wrapper">
       <div class="web-title">RichTech</div>
       <div class="user-cnt">
-        <div class="user-info" v-if="isLogin">
+        <div class="user-info" v-if="data.isLogin">
           <img class="user-avatar" src="../assets/images/user_default_grey.png">
-          <span class="user-name">Anonymous</span>
+          <span class="user-name">data.uname</span>
         </div>
-        <el-link v-else>登录</el-link>
+        <el-button type="text" @click="data.loginDialogVisible=true" v-else>登录</el-button>
+        <LoginDialog :loginDialogVisible.sync="data.loginDialogVisible"></LoginDialog>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import LoginDialog from './LoginDialog'
+import axios from 'axios'
+
 export default {
   data () {
     return {
-      isLogin: true
+      data: {
+        loginDialogVisible: false
+      }
+    }
+  },
+  created () {
+    this.getUserInfo()
+  },
+  components: {
+    'LoginDialog': LoginDialog
+  },
+  methods: {
+    getUserInfo: () => {
+      axios.get('/user/info')
+        .then(res => {
+          let info = res.data || {}
+          if (info.uid) {
+            this.data.uname = info.name
+            this.data.uid = info.uid
+          }
+        }, rejected => {
+          console.log(rejected)
+        })
+    }
+  },
+  computed: {
+    isLogin: () => {
+      return this.data.uid
     }
   }
 }
